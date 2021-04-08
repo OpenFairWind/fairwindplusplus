b32a39e687784f94c48d86cc589818cb224bb47d
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-04-08 23:18:47 +0200

    Deleted

--- apps/IFairWindApp.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Raffaele Montella on 01/04/21.
-//
-
-#include "../include/IFairWindApp.hpp"
--- extensions/plugins/FairWindPlugin.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Raffaele Montella on 04/04/21.
-//
-
-#include "../../include/FairWindPlugin.hpp"
