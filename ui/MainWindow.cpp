bc1fbb636ec39878838d690364dbee350d75c203
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-04-12 22:19:24 +0200

    Updated (moved part of the code in BottomBar, TopBar, BottomBarIcon.

--- ui/MainWindow.cpp
+++ ui/MainWindow.cpp
@@ -4,11 +4,12 @@
 
 #include <QTimer>
 #include <QLabel>
-#include <QPushButton>
+
 #include <QGridLayout>
 #include <QToolButton>
 #include "MainWindow.hpp"
-
+#include "TopBar.hpp"
+#include "BottomBar.hpp"
 #include "ui_MainWindow.h"
 #include "../core/FairWind.hpp"
 
@@ -20,104 +21,12 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     ui->setupUi(this);
+    m_topBar = new TopBar(this);
+    m_bottonBar = new BottomBar(this);
 
-    QLabel *label_Latitude = new QLabel("40°51\".221 N");
-    QLabel *label_Longitude = new QLabel("14°28\".435 E");
-
-    QWidget *widgetDummy0 = new QWidget(this);
-    widgetDummy0->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-
-    QLabel *label_Role = new QLabel("Skipper");
-
-    QWidget *widgetDummy1 = new QWidget(this);
-    widgetDummy1->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-
-    QLabel *label_DateTime = new QLabel("Mon 22 Mar 13:50");
-
-    ui->toolBarTop->addWidget(label_Latitude);
-    ui->toolBarTop->addWidget(label_Longitude);
-    ui->toolBarTop->addWidget(widgetDummy0);
-    ui->toolBarTop->addWidget(label_Role);
-    ui->toolBarTop->addWidget(widgetDummy1);
-    ui->toolBarTop->addWidget(label_DateTime);
-
-    typedef  void (MainWindow::*BottomBarIconFn)(void);
-
-    class BottomBarIcon {
-    public:
-        BottomBarIcon() {
-            name="";
-            icon="";
-            func= nullptr;
-        }
-
-        BottomBarIcon(std::string name, std::string icon) {
-            this->name=name;
-            this->icon=icon;
-            this->func= nullptr;
-        }
-        BottomBarIcon(const BottomBarIcon &bottomBarIcon)  {
-            this->name=bottomBarIcon.name;
-            this->icon=bottomBarIcon.icon;
-            this->func=bottomBarIcon.func;
-        }
-
-        BottomBarIcon(std::string name, std::string icon, BottomBarIconFn func) {
-            this->name=name;
-            this->icon=icon;
-            this->func=func;
-
-        }
-
-
-
-        std::string name;
-        std::string icon;
-        BottomBarIconFn func= nullptr;
-    };
-
-    std::vector<BottomBarIcon> bottombarIcons;
-    bottombarIcons.emplace_back("My Data",":/resources/images/icons/imagebutton_mydata.png");
-    bottombarIcons.emplace_back("MOB",":/resources/images/icons/imagebutton_mob.png");
-    bottombarIcons.emplace_back(
-                    "Apps",
-                    ":/resources/images/icons/imagebutton_apps.png",
-                    &MainWindow::toolButton_Apps_released
-
-                    );
-    bottombarIcons.emplace_back("Alarms",":/resources/images/icons/imagebutton_alarms.png");
-    bottombarIcons.emplace_back(
-                    "Settings",
-                    ":/resources/images/icons/imagebutton_settings.png",
-                    &MainWindow::toolButton_Settings_released
-
-                    );
-
-    auto *widgetDummy2 = new QWidget(this);
-    widgetDummy2->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-    ui->toolBarBottom->addWidget(widgetDummy2);
-
-    for(BottomBarIcon bottomBarIcon: bottombarIcons) {
-        auto *toolButton = new QToolButton();
-        toolButton->setText(bottomBarIcon.name.c_str());
-        toolButton->setIcon(QIcon(QString(bottomBarIcon.icon.c_str())));
-        toolButton->setIconSize(QSize(32,32));
-        toolButton->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
-        ui->toolBarBottom->addWidget(toolButton);
-        if (bottomBarIcon.func!= nullptr) {
-            connect(toolButton, &QToolButton::released, this, bottomBarIcon.func);
-        }
-    }
-
-    auto *widgetDummy3 = new QWidget(this);
-    widgetDummy3->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-
-    ui->toolBarBottom->addWidget(widgetDummy3);
 
     QGridLayout *layout = new QGridLayout(ui->scrollAreaWidgetContents_Apps);
     ui->scrollAreaWidgetContents_Apps->setLayout(layout);
-
-
 
     int cols=4,rows=2;
 
@@ -197,21 +106,14 @@ void MainWindow::toolButton_App_released()
 }
 
 
-
-void MainWindow::toolButton_Settings_released()
-{
-
-
-    ui->stackedWidget->setCurrentWidget(ui->page_Settings);
-}
-
-void MainWindow::toolButton_Apps_released()
-{
-    ui->stackedWidget->setCurrentWidget(ui->page_Apps);
-}
-
 MainWindow::~MainWindow()
 {
+    delete m_bottonBar;
+    delete m_topBar;
     delete ui;
+}
+
+Ui::MainWindow *MainWindow::getUi() {
+    return ui;
 }
 
--- ui/MainWindow.hpp
+++ ui/MainWindow.hpp
@@ -7,38 +7,39 @@
 
 #include <QMainWindow>
 #include <QMap>
-#include <QWebEngineProfile>
-
 #include <QDebug>
-#include <QPluginLoader>
+
 
 #include "../core/App.hpp"
 
+
+class BottomBar;
+class TopBar;
 
 namespace Ui {
     class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-Q_OBJECT
+class MainWindow : public QMainWindow {
+    Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+
     ~MainWindow();
+
+    Ui::MainWindow *getUi();
+
+    void toolButton_App_released();
 
 private:
     Ui::MainWindow *ui;
     QMap<QString, QWidget *> mapWidgets;
 
-
-    void toolButton_App_released();
-    void toolButton_Apps_released();
-    void toolButton_Settings_released();
-
-
-
+    TopBar *m_topBar = nullptr;
+    BottomBar *m_bottonBar = nullptr;
 };
+
 
 
 #endif //FAIRWINDS_MAINWINDOW_HPP
