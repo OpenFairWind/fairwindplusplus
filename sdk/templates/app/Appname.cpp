b5ce8d5b6f7a8fa24bbad95393527a56bf655e49
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-07-15 22:47:22 +0200

    Template for a generic app

---
+++ sdk/templates/app/Appname.cpp
@@ -0,0 +1,58 @@
+//
+// Created by __author__ on __date__.
+//
+
+#include <FairWindSdk/FairWind.hpp>
+#include "__Appname__.hpp"
+
+/*
+ * Returns the application icon
+ */
+QImage __namespace__::__appname__::__Appname__::getIcon() const {
+    return QImage(":/resources/images/icons/__appname___icon.png");
+}
+
+/*
+ * Called by the FairWind framework when the app is invoked for the first time
+ */
+QWidget *__namespace__::__appname__::__Appname__::onGui(QMainWindow *mainWindow, QMap<QString, QVariant> args) {
+
+    m_widget=new QWidget();
+    ui=new Ui::Entertainment();
+    ui->setupUi(m_widget);
+
+    auto fairwind=FairWind::getInstance();
+    auto config = getConfig();
+
+    return m_widget;
+}
+
+
+
+QString __namespace__::__appname__::__Appname__::getId() const {
+    return AppBase::getId();
+}
+
+QString __namespace__::__appname__::__Appname__::getName() const {
+    return AppBase::getName();
+}
+
+QString __namespace__::__appname__::__Appname__::getDesc() const {
+    return AppBase::getDesc();
+}
+
+void __namespace__::__appname__::__Appname__::onInit(QJsonObject *metaData) {
+    AppBase::onInit(metaData);
+}
+
+QWidget *__namespace__::__appname__::__Appname__::onSettings(QTabWidget *tabWidget) {
+    return nullptr;
+}
+
+QJsonObject __namespace__::__appname__::__Appname__::getConfig() {
+    return AppBase::getConfig();
+}
+
+QJsonObject __namespace__::__appname__::__Appname__::getMetaData() {
+    return AppBase::getMetaData();
+}
