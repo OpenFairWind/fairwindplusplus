99f3bea884ab895148cd2bfd526c364b5ba22a81
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-09-16 07:59:23 +0200

    DisplayVerticalBarGauge first implementation

--- sdk/include/FairWindSdk/displays/DisplayVerticalBarGauge.hpp
+++ sdk/include/FairWindSdk/displays/DisplayVerticalBarGauge.hpp
@@ -6,9 +6,44 @@
 #define FAIRWIND_DISPLAYVERTICALBARGAUGE_HPP
 
 
-class DisplayVerticalBarGauge {
+#include <QWidget>
+#include <FairWindSdk/FairWindSDK.hpp>
+#include <FairWindSdk/displays/DisplayBase.hpp>
+#include <FairWindSdk/IDisplay.hpp>
+#include <qcgaugewidget.h>
 
-};
+namespace Ui {
+    class DisplayVerticalBarGauge;
+}
 
+namespace fairwind::displays {
+    class FAIRWINDSDK_LIB_DECL DisplayVerticalBarGauge: public QWidget, public fairwind::displays::DisplayBase, public fairwind::displays::IDisplay {
+    Q_OBJECT
+    public:
+        explicit DisplayVerticalBarGauge(QWidget *parent = nullptr);
+        ~DisplayVerticalBarGauge();
+
+        QString getClassName() const override;
+        QImage getIcon() const override;
+        void onInit(QMap<QString, QVariant> params) override;
+        IDisplay *getNewInstance() override;
+        QWidget *onSettings() override;
+
+
+        void setLabel(QString label) override;
+        void setUnits(QString units) override;
+        void setValue(QString value) override;
+        void subscribe(QString fullPath) override;
+
+    public slots:
+
+        void update(const QJsonObject update) override;
+
+    private:
+
+        Ui::DisplayVerticalBarGauge *ui;
+
+    };
+}
 
 #endif //FAIRWIND_DISPLAYVERTICALBARGAUGE_HPP
