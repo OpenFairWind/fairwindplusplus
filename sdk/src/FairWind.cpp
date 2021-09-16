cd2faeb15ef4fba1d495f2a9d2a201bfefb8173d
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-09-16 07:59:44 +0200

    Added DisplayVerticalBarGauge

--- sdk/src/FairWind.cpp
+++ sdk/src/FairWind.cpp
@@ -14,6 +14,7 @@
 #include <FairWindSdk/displays/DisplaySingleText.hpp>
 #include <FairWindSdk/displays/DisplayDoubleText.hpp>
 #include <FairWindSdk/displays/DisplayGauge.hpp>
+#include <FairWindSdk/displays/DisplayVerticalBarGauge.hpp>
 #include <FairWindSdk/displays/DisplayChart.hpp>
 #include <FairWindSdk/displays/DisplaySimpleSwitch.hpp>
 #include <FairWindSdk/connections/SignalKWSClient.hpp>
@@ -44,6 +45,7 @@ fairwind::FairWind::FairWind() {
     registerDisplay(new displays::DisplaySingleText());
     registerDisplay(new displays::DisplayDoubleText());
     registerDisplay(new displays::DisplayGauge());
+    registerDisplay(new displays::DisplayVerticalBarGauge());
     registerDisplay(new displays::DisplayWindAngleGauge());
     registerDisplay(new displays::DisplayChart());
     registerDisplay(new displays::DisplaySimpleSwitch());
