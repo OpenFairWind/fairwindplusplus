b5c34c085014d67c3a14cdde4000fb3e19736217
Nicolas Mariniello <seldon1000@tutanota.com>
2021-11-04 15:27:10 +0100

    Documentation/lint: #2

--- sdk/include/FairWindSdk/AppBase.hpp
+++ sdk/include/FairWindSdk/AppBase.hpp
@@ -5,31 +5,30 @@
 #ifndef FAIRWIND_APPBASE_HPP
 #define FAIRWIND_APPBASE_HPP
 
-
 #include <QJsonObject>
 #include <FairWindSdk/FairWindSDK.hpp>
 
 namespace fairwind {
-    class FAIRWINDSDK_LIB_DECL AppBase {
-    public:
-        ~AppBase() = default;
+    class FAIRWINDSDK_LIB_DECL AppBase{
+            public:
+            ~AppBase() = default;
 
-        virtual QString getId() const;
-        virtual QString getName() const;
-        virtual QString getDesc() const;
-        virtual QString getVersion() const;
-        virtual QString getVendor() const;
-        virtual QString getCopyright() const;
-        virtual QString getLicense() const;
+            virtual QString getId() const;
+            virtual QString getName() const;
+            virtual QString getDesc() const;
+            virtual QString getVersion() const;
+            virtual QString getVendor() const;
+            virtual QString getCopyright() const;
+            virtual QString getLicense() const;
 
-        virtual void onInit(QJsonObject *metaData);
+            virtual void onInit(QJsonObject *metaData);
 
-        virtual QJsonObject getConfig();
-        virtual QJsonObject getMetaData();
+            virtual QJsonObject getConfig();
+            virtual QJsonObject getMetaData();
 
-    private:
-        QJsonObject m_metaData;
-        QJsonObject m_config;
+            private:
+            QJsonObject m_metaData;
+            QJsonObject m_config;
     };
 }
 
