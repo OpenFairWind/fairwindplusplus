5b4198161b36fcbb5613a9caa140856e8fd24597
Nicolas Mariniello <seldon1000@tutanota.com>
2021-11-04 15:29:11 +0100

    Documentation/lint: #16

--- sdk/include/FairWindSdk/IDisplay.hpp
+++ sdk/include/FairWindSdk/IDisplay.hpp
@@ -6,21 +6,32 @@
 #define FAIRWIND_IDISPLAY_HPP
 
 namespace fairwind::displays {
-
+    /*
+     * IDisplay
+     * Interface to build new widgets containing useful infos in FairWind
+     */
     class IDisplay {
     public:
         virtual ~IDisplay() = default;
 
+        // Returns the display's icon
         virtual QImage getIcon() const = 0;
-        virtual void onInit(QMap<QString, QVariant> params) = 0;
-        virtual IDisplay *getNewInstance()  = 0;
+
+        // Initialisation method
+        virtual void onInit(QMap <QString, QVariant> params) = 0;
+
+        // Returns a newly created instance
+        virtual IDisplay *getNewInstance() = 0;
+
+        // Method called when the display's settings are launched by the user
         virtual QWidget *onSettings() = 0;
+
+        // Returns the display's class name
         virtual QString getClassName() const = 0;
 
         //virtual bool smaller() = 0;
         //virtual bool bigger() = 0;
     };
-
 }
 
 #endif //FAIRWIND_IDISPLAY_HPP
--- sdk/include/FairWindSdk/ILayout.hpp
+++ sdk/include/FairWindSdk/ILayout.hpp
@@ -8,21 +8,35 @@
 #include <FairWindSdk/IDisplay.hpp>
 
 namespace fairwind::layouts {
-
+    /*
+     * ILayout
+     * Interface to build new visual layouts for FairWind
+     */
     class ILayout {
     public:
         virtual ~ILayout() = default;
 
+        // Returns the layout's icon
         virtual QImage getIcon() const = 0;
-        virtual void onInit(QMap<QString, QVariant> params) = 0;
-        virtual ILayout *getNewInstance()  = 0;
+
+        // Initialisation method
+        virtual void onInit(QMap <QString, QVariant> params) = 0;
+
+        // Returns a newly created instance
+        virtual ILayout *getNewInstance() = 0;
+
+        // Method called when the layout's settings are launched by the user
         virtual QWidget *onSettings() = 0;
+
+        // Returns the layout's class name
         virtual QString getClassName() const = 0;
 
+        // Returns the layout in form of Qt widget
         virtual QWidget *getWidget() = 0;
+
+        // Adds a new widget to the layout
         virtual void addDisplay(QString key, displays::IDisplay *display) = 0;
     };
-
 }
 
 #endif //FAIRWIND_ILAYOUT_HPP
