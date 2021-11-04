2ea8b7f2e36daa595bcc994c0d8ec16f82a5060d
Nicolas Mariniello <seldon1000@tutanota.com>
2021-11-04 15:28:03 +0100

    Documentation/lint: #10

--- sdk/src/displays/wind/DisplayWindAngleGauge.cpp
+++ sdk/src/displays/wind/DisplayWindAngleGauge.cpp
@@ -9,36 +9,51 @@
 
 #include "FairWindSdk/displays/DisplayWindAngleGauge.hpp"
 
-fairwind::displays::DisplayWindAngleGauge::DisplayWindAngleGauge(QWidget *parent):
-    QWidget(parent),
-    DisplayBase(),
-    ui(new Ui::DisplayWindAngleGauge) {
-
+/*
+ * DisplayWindAngleGauge - Public Constructor
+ */
+fairwind::displays::DisplayWindAngleGauge::DisplayWindAngleGauge(QWidget *parent) :
+        QWidget(parent),
+        DisplayBase(),
+        ui(new Ui::DisplayWindAngleGauge) {
+    // Setup the UI
     ui->setupUi(this);
 
-    mMinValue=-180;
-    mMaxValue=180;
-    mStep=15;
+    mMinValue = -180;
+    mMaxValue = 180;
+    mStep = 15;
 }
 
-fairwind::displays::DisplayWindAngleGauge::~DisplayWindAngleGauge() {
+/*
+ * ~DisplayWindAngleGauge - Destructor
+ */
+fairwind::displays::DisplayWindAngleGauge::~DisplayWindAngleGauge() {}
 
-}
-
+/*
+ * getIcon
+ * Returns the display's icon
+ */
 QImage fairwind::displays::DisplayWindAngleGauge::getIcon() const {
     return QImage(":resources/images/icons/signalk_icon.png");
 }
 
+/*
+ * onSettings
+ * Returns the display's settings
+ */
 QWidget *fairwind::displays::DisplayWindAngleGauge::onSettings() {
     return nullptr;
 }
 
-void fairwind::displays::DisplayWindAngleGauge::onInit(QMap<QString, QVariant> params) {
-
+/*
+ * onInit
+ * Initialization method
+ */
+void fairwind::displays::DisplayWindAngleGauge::onInit(QMap <QString, QVariant> params) {
     qDebug() << "fairwind::displays::DisplayWindAngleGauge:onInit " << params;
 
     // Setup parameters
-    QString context="${self}";
+    QString context = "${self}";
     if (params.contains("context")) {
         context = params["context"].toString();
     }
@@ -46,66 +61,60 @@ void fairwind::displays::DisplayWindAngleGauge::onInit(QMap<QString, QVariant> p
     // Create the gauge widget
     mGaugeWidget = new QcGaugeWidget;
 
-
-
     // Add an arc
-    QcArcItem *arcItem=mGaugeWidget->addArc(55);
-    arcItem->setValueRange(mMinValue,mMaxValue);
-    arcItem->setDegreeRange(mMinValue,mMaxValue);
+    QcArcItem *arcItem = mGaugeWidget->addArc(55);
+    arcItem->setValueRange(mMinValue, mMaxValue);
+    arcItem->setDegreeRange(mMinValue, mMaxValue);
 
-
-    QcDegreesItem *degreesItem=mGaugeWidget->addDegrees(65);
-    degreesItem->setValueRange(mMinValue,mMaxValue);
-    degreesItem->setDegreeRange(mMinValue,mMaxValue);
+    // Add a degree component
+    QcDegreesItem *degreesItem = mGaugeWidget->addDegrees(65);
+    degreesItem->setValueRange(mMinValue, mMaxValue);
+    degreesItem->setDegreeRange(mMinValue, mMaxValue);
     degreesItem->setDegreeOffset(90);
     degreesItem->setStep(10);
     degreesItem->setSubDegree(true);
 
-
-
     QcColorBand *clrBandPort = mGaugeWidget->addColorBand(50);
     clrBandPort->setValueRange(-60, -20);
-    clrBandPort->setDegreeRange(-60,-20);
+    clrBandPort->setDegreeRange(-60, -20);
     clrBandPort->setDegreeOffset(90);
-    QList<QPair<QColor,float>> colorsPort;
-    colorsPort.append(QPair<QColor,float>(Qt::darkRed,-60));
-    colorsPort.append(QPair<QColor,float>(Qt::darkRed,-20));
+    QList <QPair<QColor, float>> colorsPort;
+    colorsPort.append(QPair<QColor, float>(Qt::darkRed, -60));
+    colorsPort.append(QPair<QColor, float>(Qt::darkRed, -20));
     clrBandPort->setColors(colorsPort);
 
-
-    QcColorBand *clrBandStarboard= mGaugeWidget->addColorBand(50);
+    QcColorBand *clrBandStarboard = mGaugeWidget->addColorBand(50);
     clrBandStarboard->setValueRange(20, 60);
-    clrBandStarboard->setDegreeRange(20,60);
+    clrBandStarboard->setDegreeRange(20, 60);
     clrBandStarboard->setDegreeOffset(90);
-    QList<QPair<QColor,float>> colorsStarboard;
-    colorsStarboard.append(QPair<QColor,float>(Qt::darkGreen,20));
-    colorsStarboard.append(QPair<QColor,float>(Qt::darkGreen,60));
+    QList <QPair<QColor, float>> colorsStarboard;
+    colorsStarboard.append(QPair<QColor, float>(Qt::darkGreen, 20));
+    colorsStarboard.append(QPair<QColor, float>(Qt::darkGreen, 60));
     clrBandStarboard->setColors(colorsStarboard);
 
-
-    QcValuesItem *valuesItem=mGaugeWidget->addValues(80);
-    valuesItem->setValueRange(-150,150);
-    valuesItem->setDegreeRange(-150,150);
+    QcValuesItem *valuesItem = mGaugeWidget->addValues(80);
+    valuesItem->setValueRange(-150, 150);
+    valuesItem->setDegreeRange(-150, 150);
     valuesItem->setDegreeOffset(90);
     valuesItem->setStep(30);
 
+    mLabel = mGaugeWidget->addLabel(70);
 
-    mLabel=mGaugeWidget->addLabel(70);
-
-    mUnits=mGaugeWidget->addLabel(50);
+    mUnits = mGaugeWidget->addLabel(50);
 
     mValue = mGaugeWidget->addLabel(40);
 
     mNeedle = mGaugeWidget->addNeedle(60);
     mNeedle->setLabel(mValue);
     mNeedle->setColor(Qt::blue);
-    mNeedle->setValueRange(mMinValue,mMaxValue);
-    mNeedle->setDegreeRange(mMinValue,mMaxValue);
+    mNeedle->setValueRange(mMinValue, mMaxValue);
+    mNeedle->setDegreeRange(mMinValue, mMaxValue);
     mNeedle->setDegreeOffset(90);
 
     mGaugeWidget->addBackground(7);
 
-    subscribe(context+".environment.wind.angleApparent");
+    // Subscribe the get wind information
+    subscribe(context + ".environment.wind.angleApparent");
 
     if (params.contains("label")) {
         setLabel(params["label"].toString());
@@ -148,7 +157,7 @@ void fairwind::displays::DisplayWindAngleGauge::setUnits(QString units) {
 
 void fairwind::displays::DisplayWindAngleGauge::setValue(QString text) {
     mValue->setText(text);
-    double value=text.toDouble();
+    double value = text.toDouble();
     mNeedle->setCurrentValue(value);
 }
 
@@ -158,8 +167,8 @@ void fairwind::displays::DisplayWindAngleGauge::subscribe(QString fullPath) {
 
     DisplayBase::subscribe(fullPath);
     setToolTip(getDescription());
-    signalKDocument->subscribe( getFullPath(),
-                                this,SLOT(DisplayWindAngleGauge::update));
+    signalKDocument->subscribe(getFullPath(),
+                               this, SLOT(DisplayWindAngleGauge::update));
 }
 
 void fairwind::displays::DisplayWindAngleGauge::update(const QJsonObject update) {
