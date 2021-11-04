45c270c03a3e1aaebeb36259508c05114788d373
Nicolas Mariniello <seldon1000@tutanota.com>
2021-11-04 15:28:52 +0100

    Documentation/lint: #14

--- sdk/include/FairWindSdk/IApp.hpp
+++ sdk/include/FairWindSdk/IApp.hpp
@@ -13,33 +13,57 @@
 #define IID_FAIRWIND_APPS "it.uniparthenope.fairwind.apps"
 
 namespace fairwind::apps {
-
+    /*
+     * IApp
+     * Interface to build FairWind apps
+     */
     class IApp {
     public:
+        // ~IApp
         virtual ~IApp() = default;
 
+        // Initialisation method
         virtual void onInit(QJsonObject *metaData) = 0;
 
-
+        // Returns the app's id
         virtual QString getId() const = 0;
-        virtual QString getName() const = 0;
-        virtual QString getDesc() const  = 0;
-        virtual QString getVersion() const  = 0;
-        virtual QString getVendor() const  = 0;
-        virtual QString getCopyright() const  = 0;
-        virtual QString getLicense() const  = 0;
 
+        // Returns the app's name
+        virtual QString getName() const = 0;
+
+        // Returns the app's description
+        virtual QString getDesc() const = 0;
+
+        // Returns the app's version
+        virtual QString getVersion() const = 0;
+
+        // Returns the app's vendor
+        virtual QString getVendor() const = 0;
+
+        // Returns the app's copyright
+        virtual QString getCopyright() const = 0;
+
+        // Returns the app's license
+        virtual QString getLicense() const = 0;
+
+        // Returns the app's icon
         virtual QImage getIcon() const = 0;
-        virtual QWidget *onGui(QMainWindow *mainWindow, QMap<QString, QVariant> args) = 0;
+
+        // Method called when the app is first launched by the user
+        virtual QWidget *onGui(QMainWindow *mainWindow, QMap <QString, QVariant> args) = 0;
+
+        // Method called when the in-app settings are launched by the user
         virtual QWidget *onSettings(QTabWidget *tabWidget) = 0;
+
+        // Returns the app's configuration
         virtual QJsonObject getConfig() = 0;
+
+        // Returns the app's metadata
         virtual QJsonObject getMetaData() = 0;
     };
-
 }
 
-Q_DECLARE_INTERFACE(fairwind::apps::IApp, IID_FAIRWIND_APPS)
-
-
+Q_DECLARE_INTERFACE(fairwind::apps::IApp,
+IID_FAIRWIND_APPS)
 
 #endif //FAIRWIND_IAPP_HPP
