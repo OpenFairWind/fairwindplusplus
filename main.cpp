d6c1bac066dc34f467dddea4c266e25b1e73072a
Nicolas Mariniello <seldon1000@tutanota.com>
2021-11-25 07:28:41 -0800

    Fix build process ui/settings/generic/Generic.hpp was not included in the main file

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <QSplashScreen>
 
 #include <FairWindSdk/FairWind.hpp>
+#include <ui/settings/generic/Generic.hpp>
 #include <ui/settings/connections/Connections.hpp>
 #include <ui/settings/applications/Applications.hpp>
 
