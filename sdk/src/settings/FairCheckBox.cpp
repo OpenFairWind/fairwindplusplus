1cb145609fb541694d6644ed7e82e3d2c0bf2e98
Nicolas Mariniello <seldon1000@tutanota.com>
2021-12-01 08:35:20 -0800

    Added more comments

--- sdk/src/settings/FairCheckBox.cpp
+++ sdk/src/settings/FairCheckBox.cpp
@@ -10,15 +10,19 @@
 #include <FairWindSdk/IApp.hpp>
 
 void fairwind::ui::settings::FairCheckBox::setDetails(QJsonObject settings, QJsonObject values, fairwind::apps::IApp* extension) {
+    // Get the settings ID
     auto settingsID = settings["id"].toString();
 
+    // Get the settings current value
     QString checkState = values[settingsID].toString();
 
+    // Set the checkbox's state according to the current value
     if (checkState.toInt() == 0)
         this->setCheckState(Qt::CheckState::Unchecked);
     else
         this->setCheckState(Qt::CheckState::Checked);
 
+    // When the current value changes, call the updateSettings method to save the changes
     connect(this,static_cast<void (QCheckBox::*)(int state)>(&QCheckBox::stateChanged), this, [settingsID, extension, checkState]() {
         extension->updateSettings(settingsID, checkState == "0" ? "2" : "0");
     });
