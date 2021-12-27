fca1f0d884f3979ba16b559a4ed62e865497b8db
Nicolas Mariniello <seldon1000@tutanota.com>
2021-12-27 12:41:54 +0100

    first settings ui work

--- sdk/src/settings/FairCheckBox.cpp
+++ sdk/src/settings/FairCheckBox.cpp
@@ -7,24 +7,39 @@
 #include <FairWindSdk/FairWind.hpp>
 #include <FairWindSdk/IApp.hpp>
 #include <FairWindSdk/settings/FairCheckBox.hpp>
+#include <QGridLayout>
+#include <QLabel>
 
 void fairwind::ui::settings::FairCheckBox::setDetails(QJsonObject settings, QJsonObject values, fairwind::apps::IApp* extension) {
+    auto layout = new QGridLayout;
     // Get the settings ID
     auto settingsID = settings["id"].toString();
+    // Create a label
+    auto label = new QLabel(settings["displayName"].toString() + ":");
+    auto checkBox = new QCheckBox;
+
+    QFont font = QFont("", 20);
+
+    label->setFont(font);
 
     // Get the settings current value
     QString checkState = values[settingsID].toString();
 
     // Set the checkbox's state according to the current value
     if (checkState.toInt() == 0)
-        this->setCheckState(Qt::CheckState::Unchecked);
+        checkBox->setCheckState(Qt::CheckState::Unchecked);
     else
-        this->setCheckState(Qt::CheckState::Checked);
+        checkBox->setCheckState(Qt::CheckState::Checked);
 
     // When the current value changes, call the updateSettings method to save the changes
-    connect(this,static_cast<void (QCheckBox::*)(int state)>(&QCheckBox::stateChanged), this, [settingsID, extension, checkState]() {
+    connect(checkBox,static_cast<void (QCheckBox::*)(int state)>(&QCheckBox::stateChanged), this, [settingsID, extension, checkState]() {
         extension->updateSettings(settingsID, checkState == "0" ? "2" : "0");
     });
+
+    layout->addWidget(label, 0, 0);
+    layout->addWidget(checkBox, 0, 1);
+
+    this->setLayout(layout);
 }
 
 fairwind::ui::settings::ISettings *fairwind::ui::settings::FairCheckBox::getNewInstance() {
