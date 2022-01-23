f00cd6a45967974c6faf6a7acbb71798b7a5f63f
Raffaele Montella <raffaele.montella@uniparthenope.it>
2022-01-23 01:35:58 +0100

    New file

---
+++ apps/settings/browser/UIValue.cpp
@@ -0,0 +1,110 @@
+//
+// Created by Raffaele Montella on 22/01/22.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_UIValue.h" resolved
+
+#include <QLineEdit>
+#include <QRegExpValidator>
+#include <QCheckBox>
+#include "UIValue.hpp"
+#include "ui_UIValue.h"
+#include "UIArray.hpp"
+
+namespace fairwind::apps::settings::browser {
+    UIValue::UIValue(QWidget *parent, QJsonValueRef ref, QString key) :
+        QWidget(parent), ui(new Ui::UIValue), m_ref(ref), m_key(std::move(key)) {
+        ui->setupUi(this);
+
+
+        qDebug() << "UIValue::UIValue: " << m_key;
+        if (m_ref.isString() || m_ref.isBool() || m_ref.isDouble()) {
+            ui->labelKey->setText(m_key);
+
+            if (m_ref.isString()) {
+                auto *widget = new QLineEdit();
+                widget->setText(m_ref.toString());
+                ui->verticalLayout_Value->addWidget(widget);
+
+                connect(widget, &QLineEdit::textChanged, this, &UIValue::onTextChanged);
+
+                m_widget = widget;
+            } else if (m_ref.isDouble()) {
+                auto *widget = new QLineEdit();
+                widget->setValidator(new QRegExpValidator(QRegExp("[0-9]*"), widget));
+                double value = m_ref.toDouble();
+                widget->setText(QString::number(value));
+                ui->verticalLayout_Value->addWidget(widget);
+
+                connect(widget, &QLineEdit::textChanged, this, &UIValue::onNumberChanged);
+
+                m_widget = widget;
+            } else if (m_ref.isBool()) {
+                auto *widget = new QCheckBox();
+                widget->setText("");
+                widget->setChecked(m_ref.toBool());
+                ui->verticalLayout_Value->addWidget(widget);
+
+                connect(widget, &QCheckBox::stateChanged, this, &UIValue::onBoolChanged);
+
+                m_widget = widget;
+            }
+        } else if (m_ref.isArray() || m_ref.isObject()) {
+
+            if (m_ref.isArray()) {
+                auto *uiArray = new UIArray(nullptr, m_ref, m_key);
+                ui->verticalLayout_Value->addWidget(uiArray);
+                connect(uiArray, &UIArray::changed, this, &UIValue::onArrayChanged);
+                m_widget = uiArray;
+
+            } else if (m_ref.isObject()) {
+                auto *uiObject = new UIObject(nullptr, m_ref, m_key);
+                ui->verticalLayout_Value->addWidget(uiObject);
+                connect(uiObject, &UIObject::changed, this, &UIValue::onObjectChanged);
+                m_widget = uiObject;
+            }
+        }
+    }
+
+    UIValue::~UIValue() {
+        delete ui;
+    }
+
+    void UIValue::onTextChanged() {
+        qDebug() << "UIValue::onTextChanged()";
+        auto *widget = (QLineEdit *) m_widget;
+        QString text = widget->text();
+        m_ref = text;
+        qDebug() << "m_ref:" << m_ref;
+        emit changed();
+
+    }
+
+    void UIValue::onNumberChanged() {
+        qDebug() << "UIValue::onNumberChanged()";
+        auto *widget = (QLineEdit *) m_widget;
+        double number = widget->text().toDouble();
+        m_ref = number;
+        qDebug() << "m_ref:" << m_ref;
+        emit changed();
+
+    }
+
+    void UIValue::onBoolChanged(int state) {
+        qDebug() << "UIValue::onBoolChanged(" << state << ")";
+        auto *widget = (QCheckBox *) m_widget;
+        m_ref = widget->isChecked();
+        qDebug() << "m_ref:" << m_ref;
+        emit changed();
+    }
+
+    void UIValue::onArrayChanged() {
+        qDebug() << "UIObject::onArrayChanged()";
+        emit changed();
+    }
+
+    void UIValue::onObjectChanged() {
+        qDebug() << "UIObject::onObjectChanged()";
+        emit changed();
+    }
+} // fairwind::apps::settings::browser
---
+++ apps/settings/browser/UIValue.hpp
@@ -0,0 +1,50 @@
+//
+// Created by Raffaele Montella on 22/01/22.
+//
+
+#ifndef FAIRWIND_UIVALUE_HPP
+#define FAIRWIND_UIVALUE_HPP
+
+#include <QWidget>
+#include <QJsonValueRef>
+#include <QJsonObject>
+
+namespace fairwind::apps::settings::browser {
+    QT_BEGIN_NAMESPACE
+    namespace Ui { class UIValue; }
+    QT_END_NAMESPACE
+
+    class UIValue : public QWidget {
+    Q_OBJECT
+
+    public:
+        explicit UIValue(QWidget *parent, QJsonValueRef ref, QString key);
+
+        ~UIValue() override;
+
+    signals:
+
+        void changed();
+
+    public slots:
+
+        void onTextChanged();
+
+        void onNumberChanged();
+
+        void onBoolChanged(int state);
+
+        void onArrayChanged();
+        void onObjectChanged();
+
+    private:
+        QJsonValueRef m_ref;
+        QString m_key;
+        QWidget *m_widget;
+        QJsonObject m_jsonObject;
+
+        Ui::UIValue *ui;
+    };
+} // fairwind::apps::settings::browser
+
+#endif //FAIRWIND_UIVALUE_HPP
