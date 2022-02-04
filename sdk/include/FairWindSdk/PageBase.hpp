de973f0c6bd7bc538d79d17fd5d7b23086ae711e
Raffaele Montella <raffaele.montella@uniparthenope.it>
2022-02-04 13:51:41 +0100

    Added application lifecycle methods. Added onConfigChanged method.

--- sdk/include/FairWindSdk/PageBase.hpp
+++ sdk/include/FairWindSdk/PageBase.hpp
@@ -8,17 +8,38 @@
 #include <QJsonObject>
 #include <FairWindSdk/FairWindSDK.hpp>
 #include <FairWindSdk/IFairWindApp.hpp>
+#include <FairWindSdk/FairWindApp.hpp>
 
-
-namespace fairwind {
+namespace fairwind::apps {
     class FAIRWINDSDK_LIB_DECL PageBase: public QWidget {
     Q_OBJECT
 
     public:
-            PageBase(QWidget *parent = nullptr, apps::FairWindApp *fairWindApp = nullptr);
+            PageBase(QWidget *parent = nullptr, FairWindApp *fairWindApp = nullptr);
             ~PageBase() = default;
 
-            apps::FairWindApp *getFairWindApp();
+            FairWindApp *getFairWindApp();
+
+        // Called by the app loader when the app is loaded
+        virtual void onCreate();
+
+        // Method called when the app is first launched by the user (only one time in the lifecycle)
+        virtual void onStart();
+
+        // Called when the app is going to be in foreground
+        virtual void onResume();
+
+        // Called when the app is going to be in background
+        virtual void onPause();
+
+        // Called when the app is going to be stopped
+        virtual void onStop();
+
+        // Called when the app is going to be unloaded by the system
+        virtual void onDestroy();
+
+        // Called when the config changed
+        virtual void onConfigChanged();
 
     private:
             apps::FairWindApp *m_fairWindApp;
--- sdk/src/PageBase.cpp
+++ sdk/src/PageBase.cpp
@@ -4,11 +4,41 @@
 
 #include "PageBase.hpp"
 
-fairwind::PageBase::PageBase(QWidget *parent, apps::FairWindApp *fairWindApp):
-        QWidget(parent) {
-    m_fairWindApp=fairWindApp;
-}
+namespace fairwind::apps {
+    PageBase::PageBase(QWidget *parent, apps::FairWindApp *fairWindApp) :
+            QWidget(parent) {
+        m_fairWindApp = fairWindApp;
+    }
 
-fairwind::apps::FairWindApp *fairwind::PageBase::getFairWindApp() {
-    return m_fairWindApp;
+    FairWindApp *PageBase::getFairWindApp() {
+        return m_fairWindApp;
+    }
+
+    void PageBase::onCreate() {
+
+    }
+
+    void PageBase::onStart() {
+
+    }
+
+    void PageBase::onResume() {
+
+    }
+
+    void PageBase::onPause() {
+
+    }
+
+    void PageBase::onStop() {
+
+    }
+
+    void PageBase::onDestroy() {
+
+    }
+
+    void PageBase::onConfigChanged() {
+
+    }
 }
